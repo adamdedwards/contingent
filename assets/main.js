4d5f5bca9d0d62bdaf48427eb070717954933a9d
adamdedwards <3643320+adamdedwards@users.noreply.github.com>
2019-06-07 12:32:55 -0500

    shift variable organization to game state rather than individual vars

--- assets/main.js
+++ assets/main.js
@@ -6,27 +6,28 @@ var time = 0.0;
 var semester = 0.0;
 var anxiety = 0.0;
 
-var word =      {r: 0.0, lim: 100.0, dt: 0.0};
-var sent =      {r: 0.0, lim: 20.0, dt: 0.0, cost: 10.0};
-var graf =      {r: 0.0, lim: 40.0, dt: 0.0, cost: 10.0};
-var draft =     {r: 0.0, lim: 10.0, dt: 0.0, cost: 20.0};
-var chapter =   {r: 0.0, lim: 10.0, dt: 0.0, cost: 10000.0};
-var diss =      {r: 0.0, lim: 1.0, dt: 0.0, cost: 5.0};
-var monograph = {r: 0.0, lim: 1.0, dt: 0.0, cost: 10.0};
-var volume =    {r: 0.0, lim: 10.0, dt: 0.0, cost: 10.0};
-var outline =   {r: 0.0, lim: 0.0, dt: 0.0, cost: 10.0};
-var bed =       {r: 0.0, lim: 0.0, dt: 0.0, cost: 10.0};
-var money =     {r: 0.0, lim: 100.0, dt: 0.0, cost: 0.0};
+var state = {
+word :      {r: 0.0, lim: 100.0, dt: 0.0},
+sent :      {r: 0.0, lim: 20.0, dt: 0.0, cost: 10.0},
+graf :      {r: 0.0, lim: 40.0, dt: 0.0, cost: 10.0},
+draft :     {r: 0.0, lim: 10.0, dt: 0.0, cost: 20.0},
+chapter :   {r: 0.0, lim: 10.0, dt: 0.0, cost: 10000.0},
+diss :      {r: 0.0, lim: 1.0, dt: 0.0, cost: 5.0},
+monograph : {r: 0.0, lim: 1.0, dt: 0.0, cost: 10.0},
+volume :    {r: 0.0, lim: 10.0, dt: 0.0, cost: 10.0},
+outline :   {r: 0.0, lim: 0.0, dt: 0.0, cost: 10.0},
+money :     {r: 0.0, lim: 100.0, dt: 0.0, cost: 0.0},
+};
 
 function save() {
   localStorage.setItem('time', JSON.stringify(time));
-  localStorage.setItem('word', JSON.stringify(word));
+  localStorage.setItem('state', JSON.stringify(state));
 }
 
 function load() {
     if(localStorage.length > 0){
   time = JSON.parse(localStorage.getItem('time'));
-  word = JSON.parse(localStorage.getItem('word'));
+  state = JSON.parse(localStorage.getItem('state'));
 };
 }
 
@@ -40,79 +41,79 @@ function init() {
     // STOCKS
     document.getElementById("time_stock").innerHTML = time;
 
-    document.getElementById("word_stock").innerHTML = word.r.toFixed(precision);
-    document.getElementById("graf_stock").innerHTML = graf.r.toFixed(precision);
-    document.getElementById("draft_stock").innerHTML = draft.r.toFixed(precision);
-    document.getElementById("chapter_stock").innerHTML = chapter.r.toFixed(precision);
-    document.getElementById("dissertation_stock").innerHTML = diss.r.toFixed(precision);
-    document.getElementById("monograph_stock").innerHTML = monograph.r.toFixed(precision);
-    document.getElementById("volume_stock").innerHTML = volume.r.toFixed(precision);
-    document.getElementById("outline_stock").innerHTML = outline.r.toFixed(0);
+    document.getElementById("word_stock").innerHTML = state.word.r.toFixed(precision);
+    document.getElementById("graf_stock").innerHTML = state.graf.r.toFixed(precision);
+    document.getElementById("draft_stock").innerHTML = state.draft.r.toFixed(precision);
+    document.getElementById("chapter_stock").innerHTML = state.chapter.r.toFixed(precision);
+    document.getElementById("dissertation_stock").innerHTML = state.diss.r.toFixed(precision);
+    document.getElementById("monograph_stock").innerHTML = state.monograph.r.toFixed(precision);
+    document.getElementById("volume_stock").innerHTML = state.volume.r.toFixed(precision);
+    document.getElementById("outline_stock").innerHTML = state.outline.r.toFixed(0);
 
     // LIMITS
-    document.getElementById("word_limit").innerHTML = word.lim;
-    document.getElementById("graf_limit").innerHTML = graf.lim;
-    document.getElementById("draft_limit").innerHTML = draft.lim;
-    document.getElementById("volume_limit").innerHTML = volume.lim;
-    document.getElementById("chapter_limit").innerHTML = chapter.lim;
-    document.getElementById("dissertation_limit").innerHTML = diss.lim;
-    document.getElementById("monograph_limit").innerHTML = monograph.lim;
+    document.getElementById("word_limit").innerHTML = state.word.lim;
+    document.getElementById("graf_limit").innerHTML = state.graf.lim;
+    document.getElementById("draft_limit").innerHTML = state.draft.lim;
+    document.getElementById("volume_limit").innerHTML = state.volume.lim;
+    document.getElementById("chapter_limit").innerHTML = state.chapter.lim;
+    document.getElementById("dissertation_limit").innerHTML = state.diss.lim;
+    document.getElementById("monograph_limit").innerHTML = state.monograph.lim;
 
     // FLOWS
-    document.getElementById("word_dt").innerHTML = word.dt.toFixed(precision); // multiply by (1000/tick) for per sec rate
-    document.getElementById("graf_dt").innerHTML = graf.dt.toFixed(precision);
-    document.getElementById("draft_dt").innerHTML = draft.dt.toFixed(precision);
-    document.getElementById("chapter_dt").innerHTML = chapter.dt.toFixed(precision);
-    document.getElementById("dissertation_dt").innerHTML = diss.dt.toFixed(precision);
-    document.getElementById("monograph_dt").innerHTML = monograph.dt.toFixed(precision);
-    document.getElementById("volume_dt").innerHTML = volume.dt.toFixed(precision);
+    document.getElementById("word_dt").innerHTML = state.word.dt.toFixed(precision); // multiply by (1000/tick) for per sec rate
+    document.getElementById("graf_dt").innerHTML = state.graf.dt.toFixed(precision);
+    document.getElementById("draft_dt").innerHTML = state.draft.dt.toFixed(precision);
+    document.getElementById("chapter_dt").innerHTML = state.chapter.dt.toFixed(precision);
+    document.getElementById("dissertation_dt").innerHTML = state.diss.dt.toFixed(precision);
+    document.getElementById("monograph_dt").innerHTML = state.monograph.dt.toFixed(precision);
+    document.getElementById("volume_dt").innerHTML = state.volume.dt.toFixed(precision);
 
    // COSTS
-   document.getElementById("graf_cost").innerHTML = graf.cost.toFixed(precision);
-   document.getElementById("draft_cost").innerHTML = draft.cost.toFixed(precision);
-   document.getElementById("outline_cost").innerHTML = outline.cost.toFixed(precision);
+   document.getElementById("graf_cost").innerHTML = state.graf.cost.toFixed(precision);
+   document.getElementById("draft_cost").innerHTML = state.draft.cost.toFixed(precision);
+   document.getElementById("outline_cost").innerHTML = state.outline.cost.toFixed(precision);
 };
 
 window.setInterval(function(){
     update_time();
-    update_word(word.dt);
-    update_graf(graf.dt);
-    update_draft(draft.dt);
-    update_chapter(chapter.dt);
-    update_diss(diss.dt);
-    update_monograph(monograph.dt);
-    update_volume(volume.dt);
+    update_word(state.word.dt);
+    update_graf(state.graf.dt);
+    update_draft(state.draft.dt);
+    update_chapter(state.chapter.dt);
+    update_diss(state.diss.dt);
+    update_monograph(state.monograph.dt);
+    update_volume(state.volume.dt);
     update_costs();
     update_flows();
 }, tick*1);
 
-function inc_word(n){ n=1;  word.r = Math.round(word.r*1000 + n*1000)/1000; };
+function inc_word(n){ n=1;  state.word.r = Math.round(state.word.r*1000 + n*1000)/1000; };
 
 function update_flows(){
-    word.dt =         0.0 + outline.r*0.01;
-    graf.dt =         0.0;
-    draft.dt =        0.0;
-    chapter.dt =      0.0;
-    diss.dt =         0.0;
-    monograph.dt =    0.0;
-    volume.dt =       0.0;
+    state.word.dt =         0.0 + state.outline.r*0.01;
+    state.graf.dt =         0.0;
+    state.draft.dt =        0.0;
+    state.chapter.dt =      0.0;
+    state.diss.dt =         0.0;
+    state.monograph.dt =    0.0;
+    state.volume.dt =       0.0;
 
-    document.getElementById("word_dt").innerHTML =         word.dt.toFixed(precision);
-    document.getElementById("graf_dt").innerHTML =         graf.dt.toFixed(precision);
-    document.getElementById("draft_dt").innerHTML =        draft.dt.toFixed(precision);
-    document.getElementById("chapter_dt").innerHTML =      chapter.dt.toFixed(precision);
-    document.getElementById("dissertation_dt").innerHTML = diss.dt.toFixed(precision);
-    document.getElementById("monograph_dt").innerHTML =    monograph.dt.toFixed(precision);
-    document.getElementById("volume_dt").innerHTML =       volume.dt.toFixed(precision);
+    document.getElementById("word_dt").innerHTML =         state.word.dt.toFixed(precision);
+    document.getElementById("graf_dt").innerHTML =         state.graf.dt.toFixed(precision);
+    document.getElementById("draft_dt").innerHTML =        state.draft.dt.toFixed(precision);
+    document.getElementById("chapter_dt").innerHTML =      state.chapter.dt.toFixed(precision);
+    document.getElementById("dissertation_dt").innerHTML = state.diss.dt.toFixed(precision);
+    document.getElementById("monograph_dt").innerHTML =    state.monograph.dt.toFixed(precision);
+    document.getElementById("volume_dt").innerHTML =       state.volume.dt.toFixed(precision);
 }
 
 function update_costs(){
-    document.getElementById("graf_cost").innerHTML =         graf.cost.toFixed(precision);
-    document.getElementById("draft_cost").innerHTML =        draft.cost.toFixed(precision);
-    document.getElementById("chapter_cost").innerHTML =      chapter.cost.toFixed(precision);
-    document.getElementById("dissertation_cost").innerHTML = diss.cost.toFixed(precision);
-    document.getElementById("monograph_cost").innerHTML =    monograph.cost.toFixed(precision);
-    document.getElementById("outline_cost").innerHTML =      outline.cost.toFixed(precision);
+    document.getElementById("graf_cost").innerHTML =         state.graf.cost.toFixed(precision);
+    document.getElementById("draft_cost").innerHTML =        state.draft.cost.toFixed(precision);
+    document.getElementById("chapter_cost").innerHTML =      state.chapter.cost.toFixed(precision);
+    document.getElementById("dissertation_cost").innerHTML = state.diss.cost.toFixed(precision);
+    document.getElementById("monograph_cost").innerHTML =    state.monograph.cost.toFixed(precision);
+    document.getElementById("outline_cost").innerHTML =      state.outline.cost.toFixed(precision);
 }
 
 function update_time(){
@@ -129,39 +130,39 @@ function update_time(){
 }
 
 function inc_graf(){
-    if(word.r >= graf.cost) {
-        word.r = word.r - graf.cost;
-        graf.r = graf.r + 1;
-        graf.cost = 10 + graf.r*1.05;
+    if(state.word.r >= state.graf.cost) {
+        state.word.r = state.word.r - state.graf.cost;
+        state.graf.r = state.graf.r + 1;
+        state.graf.cost = 10 + state.graf.r*1.05;
 
-        document.getElementById("word_stock").innerHTML = word.r.toFixed(precision);
-        document.getElementById("graf_stock").innerHTML = graf.r.toFixed(precision);
-        document.getElementById("graf_cost").innerHTML = graf.cost.toFixed(precision);
+        document.getElementById("word_stock").innerHTML = state.word.r.toFixed(precision);
+        document.getElementById("graf_stock").innerHTML = state.graf.r.toFixed(precision);
+        document.getElementById("graf_cost").innerHTML = state.graf.cost.toFixed(precision);
     }
 };
 
 function inc_draft(n){
-    if(graf.r >= draft.cost) {
-        graf.r = graf.r - draft.cost;
-        draft.r = draft.r + 1;
-        draft.cost = 10 + draft.r*1.05;
+    if(state.graf.r >= state.draft.cost) {
+        state.graf.r = state.graf.r - state.draft.cost;
+        state.draft.r = state.draft.r + 1;
+        state.draft.cost = 10 + state.draft.r*1.05;
 
-        document.getElementById("graf_stock").innerHTML = graf.r.toFixed(precision);
-        document.getElementById("draft_stock").innerHTML = draft.r.toFixed(precision);
-        document.getElementById("draft_cost").innerHTML = draft.cost.toFixed(precision);
+        document.getElementById("graf_stock").innerHTML = state.graf.r.toFixed(precision);
+        document.getElementById("draft_stock").innerHTML = state.draft.r.toFixed(precision);
+        document.getElementById("draft_cost").innerHTML = state.draft.cost.toFixed(precision);
     }
 };
 
 function add_outline(){
-    if(word.r >= outline.cost) {
-        word.r = word.r - outline.cost;
+    if(state.word.r >= state.outline.cost) {
+        state.word.r = state.word.r - state.outline.cost;
         word.lim = word.lim + 10;
-        outline.r = outline.r + 1;
-        outline.cost = 10 + outline.r*1.05;
+        state.outline.r = state.outline.r + 1;
+        state.outline.cost = 10 + state.outline.r*1.05;
 
-        document.getElementById("word_stock").innerHTML = word.r.toFixed(precision);
-        document.getElementById("word_limit").innerHTML = word.lim.toFixed(precision);        document.getElementById("outline_stock").innerHTML = outline.r.toFixed(0);
-        document.getElementById("outline_cost").innerHTML = outline.cost.toFixed(precision);
+        document.getElementById("word_stock").innerHTML = state.word.r.toFixed(precision);
+        document.getElementById("word_limit").innerHTML = state.word.lim.toFixed(precision);        document.getElementById("outline_stock").innerHTML = state.outline.r.toFixed(0);
+        document.getElementById("outline_cost").innerHTML = state.outline.cost.toFixed(precision);
     }
 };
 
@@ -171,101 +172,101 @@ function add_outline(){
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function update_word(n){
-    word.r = Math.round(word.r*1000 + n*1000)/1000;
-    if(word.r > word.lim) {
-        word.r = word.lim;
+    state.word.r = Math.round(state.word.r*1000 + n*1000)/1000;
+    if(state.word.r > state.word.lim) {
+        state.word.r = state.word.lim;
     }
-    if(word.r > 0) {
-        document.getElementById("word_stock").innerHTML = word.r.toFixed(precision);
+    if(state.word.r > 0) {
+        document.getElementById("word_stock").innerHTML = state.word.r.toFixed(precision);
     }
     else {
-        word.r = 0;
+        state.word.r = 0;
         document.getElementById("word_stock").innerHTML = '0';
     }
 };
 
 function update_graf(n){
-    graf.r = Math.round(graf.r*1000 + n*1000)/1000;
-    if(graf.r > graf.lim) {
-        graf.r = graf.lim;
+    state.graf.r = Math.round(state.graf.r*1000 + n*1000)/1000;
+    if(state.graf.r > state.graf.lim) {
+        state.graf.r = state.graf.lim;
     }
-    if(graf.r > 0) {
-        document.getElementById("graf_stock").innerHTML = graf.r.toFixed(precision);
+    if(state.graf.r > 0) {
+        document.getElementById("graf_stock").innerHTML = state.graf.r.toFixed(precision);
     }
     else {
-        graf.r = 0;
+        state.graf.r = 0;
         document.getElementById("graf_stock").innerHTML = '0';
     }
 };
 
 function update_draft(n){
-    draft.r = Math.round(draft.r*1000 + n*1000)/1000;
-    if(draft.r > draft.lim) {
-        draft.r = draft.lim;
+    state.draft.r = Math.round(state.draft.r*1000 + n*1000)/1000;
+    if(state.draft.r > state.draft.lim) {
+        state.draft.r = state.draft.lim;
     }
-    if(draft.r > 0) {
-        document.getElementById("draft_stock").innerHTML = draft.r.toFixed(precision);
+    if(state.draft.r > 0) {
+        document.getElementById("draft_stock").innerHTML = state.draft.r.toFixed(precision);
     }
     else {
-        draft.r = 0;
+        state.draft.r = 0;
         document.getElementById("draft_stock").innerHTML = '0';
     }
 };
 
 
 function update_chapter(n){
-    chapter.r = Math.round(chapter.r*1000 + n*1000)/1000;
-    if(chapter.r > chapter.lim) {
-        chapter.r = chapter.lim;
+    state.chapter.r = Math.round(state.chapter.r*1000 + n*1000)/1000;
+    if(state.chapter.r > state.chapter.lim) {
+        state.chapter.r = state.chapter.lim;
     }
-    if(chapter.r > 0) {
-        document.getElementById("chapter_stock").innerHTML = chapter.r.toFixed(precision);
+    if(state.chapter.r > 0) {
+        document.getElementById("chapter_stock").innerHTML = state.chapter.r.toFixed(precision);
     }
     else {
-        chapter.r = 0;
+        state.chapter.r = 0;
         document.getElementById("chapter_stock").innerHTML = '0';
     }
 };
 
 function update_diss(n){
-    diss.r = Math.round(diss.r*1000 + n*1000)/1000;
-    if(diss.r > diss.lim) {
-        diss.r = diss.lim;
+    state.diss.r = Math.round(state.diss.r*1000 + n*1000)/1000;
+    if(state.diss.r > state.diss.lim) {
+        state.diss.r = state.diss.lim;
     }
-    if(diss.r > 0) {
-        document.getElementById("dissertation_stock").innerHTML = diss.r.toFixed(precision);
+    if(state.diss.r > 0) {
+        document.getElementById("dissertation_stock").innerHTML = state.diss.r.toFixed(precision);
     }
     else {
-        diss.r = 0;
+        state.diss.r = 0;
         document.getElementById("dissertation_stock").innerHTML = '0';
     }
 };
 
 
 function update_monograph(n){
-    monograph.r = Math.round(monograph.r*1000 + n*1000)/1000;
-    if(monograph.r > monograph.lim) {
-        monograph.r = monograph.lim;
+    state.monograph.r = Math.round(state.monograph.r*1000 + n*1000)/1000;
+    if(state.monograph.r > state.monograph.lim) {
+        state.monograph.r = state.monograph.lim;
     }
-    if(monograph.r > 0) {
-        document.getElementById("monograph_stock").innerHTML = monograph.r.toFixed(precision);
+    if(state.monograph.r > 0) {
+        document.getElementById("monograph_stock").innerHTML = state.monograph.r.toFixed(precision);
     }
     else {
-        monograph.r = 0;
+        state.monograph.r = 0;
         document.getElementById("monograph_stock").innerHTML = '0';
     }
 };
 
 function update_volume(n){
-    volume.r = Math.round(volume.r*1000 + n*1000)/1000;
-    if(volume.r > volume.lim) {
-        volume.r = volume.lim;
+    state.volume.r = Math.round(state.volume.r*1000 + n*1000)/1000;
+    if(state.volume.r > state.volume.lim) {
+        state.volume.r = state.volume.lim;
     }
-    if(volume.r > 0) {
-        document.getElementById("volume_stock").innerHTML = volume.r.toFixed(precision);
+    if(state.volume.r > 0) {
+        document.getElementById("volume_stock").innerHTML = state.volume.r.toFixed(precision);
     }
     else {
-        volume.r = 0;
+        state.volume.r = 0;
         document.getElementById("volume_stock").innerHTML = '0';
     }
 };
